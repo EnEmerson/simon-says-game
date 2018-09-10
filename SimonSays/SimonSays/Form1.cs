dbb18438a8c47c8d7c5f133cf1a6a6283b9aa5b5
En Emerson <007emersone@gmail.com>
2018-09-10 11:32:00 -0500

    add checks for whether comp/player turn and change button flash length accordingly

--- SimonSays/SimonSays/Form1.cs
+++ SimonSays/SimonSays/Form1.cs
@@ -16,20 +16,26 @@ namespace SimonSays
         List<string> playerPick = new List<string>();
         List<string> colors = new List<string>();
         Timer timer = new Timer();
-        bool compTurn, playerTurn;
+        bool compTurn;
 
         public Form1()
         {
             InitializeComponent();
+
             greenButton.BackColor = Color.Green;
             redButton.BackColor = Color.Red;
             yellowButton.BackColor = Color.Yellow;
             blueButton.BackColor = Color.Blue;
+
             greenButton.Text = "";
             redButton.Text = "";
             yellowButton.Text = "";
             blueButton.Text = "";
+
             colors.Clear();
+            compPick.Clear();
+            playerPick.Clear();
+
             timer.Interval = 1000;
 
             colors.Add("green");
@@ -38,22 +44,51 @@ namespace SimonSays
             colors.Add("blue");
 
             compTurn = true;
-            playerTurn = false;
-
-            compPick.Clear();
-            playerPick.Clear();
 
             computerPick();
+        }
+
+        private void checkAccuracy()
+        {
+            //this is where i will check if the arrays match or not
         }
 
         private void computerPick()
         {
             Random rand = new Random();
-            int upper = colors.Count() + 1;
-            int compChoice = rand.Next(1,upper);
-            compPick.Add(colors[compChoice-1].ToString());
+            do
+            {
+                int upper = colors.Count() + 1;
+                int compChoice = rand.Next(1, upper);
+                compPick.Add(colors[compChoice - 1].ToString());
 
-            Console.WriteLine("Computer picked: "+compChoice.ToString());
+                Console.WriteLine("Computer picked: " + compChoice.ToString());
+
+                switch (compChoice)
+                {
+                    case 1:
+                        greenButton_Click(new object(), new EventArgs());
+                        break;
+                    case 2:
+                        redButton_Click(new object(), new EventArgs());
+                        break;
+                    case 3:
+                        yellowButton_Click(new object(), new EventArgs());
+                        break;
+                    case 4:
+                        blueButton_Click(new object(), new EventArgs());
+                        break;
+                    default:
+                        MessageBox.Show("something went wrong.");
+                        return;
+                }
+
+                if(compPick.Count() > playerPick.Count())
+                {
+                    compTurn = false;
+                }
+            }
+            while (compTurn);
         }
         private void timerTick(object sender, EventArgs e)
         {
@@ -63,63 +98,77 @@ namespace SimonSays
 
         private void greenButton_Click(object sender, EventArgs e)
         {
-            timer.Tick += timerTick;
-            timer.Start();
+            startTimer();
 
-            disableButtons();
-
-            if(greenButton.Enabled == false)
+            if (greenButton.Enabled == false)
             {
                 greenButton.BackColor = Color.White;
             }
-
-            playerPick.Add("green");
-
+            if(!compTurn)
+            {
+                playerPick.Add("green");
+                checkAccuracy();
+            }
         }
 
         private void redButton_Click(object sender, EventArgs e)
         {
-            timer.Tick += timerTick;
-            timer.Start();
-
-            disableButtons();
+            startTimer();
 
             if (redButton.Enabled == false)
             {
                 redButton.BackColor = Color.White;
             }
-
-            playerPick.Add("red");
+            if (!compTurn)
+            {
+                playerPick.Add("red");
+                checkAccuracy();
+            }
         }
 
         private void yellowButton_Click(object sender, EventArgs e)
         {
-            timer.Tick += timerTick;
-            timer.Start();
-
-            disableButtons();
+            startTimer();
 
             if (yellowButton.Enabled == false)
             {
                 yellowButton.BackColor = Color.White;
             }
-
-            playerPick.Add("yellow");
+            if (!compTurn)
+            {
+                playerPick.Add("yellow");
+                checkAccuracy();
+            }
         }
 
         private void blueButton_Click(object sender, EventArgs e)
         {
-            timer.Tick += timerTick;
-            timer.Start();
-
-            disableButtons();
+            startTimer();
 
             if (blueButton.Enabled == false)
             {
                 blueButton.BackColor = Color.White;
             }
+            if (!compTurn)
+            {
+                playerPick.Add("blue");
+                checkAccuracy();
+            }
+        }
 
-            playerPick.Add("blue");
+        private void startTimer()
+        {
+            if(compTurn)
+            {
+                timer.Interval = 1000;
+            }
+            if(!compTurn)
+            {
+                timer.Interval = 100;
+            }
+            timer.Tick += timerTick;
+            timer.Start();
+            disableButtons();
         }
 
         private void disableButtons()
